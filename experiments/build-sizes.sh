3dd5c6cd7fdf4fc32026ea7b02620a188b467756
Carl Seifert <115627588+carl-tud@users.noreply.github.com>
2026-01-16 16:49:33 +0000

    add build size experiment

---
+++ experiments/build-sizes.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+metric=size
+host_name=$(uname -n)
+host_arch=$(uname -m)
+kernel_name=$(uname -s)
+kernel_ver=$(uname -r)
+
+TARGET_DIR="target-size"
+
+OS="$(uname)"
+
+size_json() {
+    CARGO_TARGET_DIR=$TARGET_DIR cargo build --release $1 --example pagerank
+    s=$(size $TARGET_DIR/release/examples/pagerank)
+    if [ "$OS" = "Darwin" ]; then
+        echo "$s" | awk 'NR==2 { print "{ ram: " $2 ", rom: " $1 " }" }'
+    else
+        echo "$s" | awk 'NR==2 { print "{ ram: " $2 + $3 ", rom: " $1 + $2 " }" }'
+    fi
+}
+
+baseline=$(size_json "")
+with_shmem=$(size_json "--features shared-memory")
+with_tcp=$(size_json "--features tcp")
+with_tls=$(size_json "--features tls")
+with_quic=$(size_json "--features quic")
+
+cat <<EOF
+{
+    command: ["cargo", "build", "--release"],
+    metric: "$metric",
+    host: {
+        name: "$host_name",
+        architecture: "$host_arch",
+        kernel: {
+            name: "$kernel_name",
+            version: "$kernel_ver"
+        }
+    },
+    results: {
+        baseline: $baseline,
+        shmem: $with_shmem,
+        tcp: $with_tcp,
+        tls: $with_tls,
+        quic: $with_quic,
+    }
+}
+EOF
